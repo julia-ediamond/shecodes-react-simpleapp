6a3899ef422d1b0800d555b9beb03652f2ca8ad9
iusharnina@gmail.com <zjulia679@gmail.com>
2021-10-12 18:51:54 +1000

    Added state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
+  
+  const [message, setMessage] = React.useState("JavaScript is cool")
+  function handleClick() { 
+    setMessage("My new message")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+  <h1>{ message }</h1>
+  <button onClick={handleClick}>Update the message</button>
+  </div> 
+  )
 }
+
+
+
 
 export default App;
